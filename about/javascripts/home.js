c03e25efe948f11a0cd45b1f5642a33a32cba467
Dakoda Wogan <d.dakoda@yahoo.com>
2015-10-15 19:37:37 -0400

    stashing for now

--- about/javascripts/home.js
+++ about/javascripts/home.js
@@ -3,13 +3,12 @@ $(document).ready(function(){
 
   $('.hamburger-toggle').on("click", function(evt){evt.preventDefault();
 
-    $('.hamburger').slideToggle("slow");
+    $('.hamburger').slideToggle("expand");
   });
 
-// var dakodaImages = ['milo.jpg', 'ak.jpg', 'ak.jpg', 'steven.jpg'];
-//     var dakoda = dakodaImages[Math.floor(Math.random() * dakodaImages.length)];
-//     $('#profile-pic').attr('src', '/img/' + dakoda);
-//   });
+  var dakodaImages = ['milo.jpg', 'pi.jpg', 'ak.jpg', 'steven.jpg'];
+  var dakoda = dakodaImages[Math.floor(Math.random() * dakodaImages.length)];
+    $('#profile-pic').attr('src', 'img/' + dakoda);
 
   var height = $(window).height();
     $('#home').height(height);
@@ -21,3 +20,5 @@ $(document).ready(function(){
       $('#about').height(height);
   });
 })
+
+// $('.hamburger').slideToggle("fast");
