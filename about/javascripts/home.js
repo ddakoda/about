ff3b3259b517fb3f3428ac1c2e135b497b2b9579
Dakoda Wogan <d.dakoda@yahoo.com>
2015-10-19 18:45:14 -0400

    sme new things work, some new problems happened because those things work now

--- about/javascripts/home.js
+++ about/javascripts/home.js
@@ -8,17 +8,47 @@ $(document).ready(function(){
 
   var dakodaImages = ['milo.jpg', 'pi.jpg', 'ak.jpg', 'steven.jpg'];
   var dakoda = dakodaImages[Math.floor(Math.random() * dakodaImages.length)];
-    $('#profile-pic').attr('src', 'img/' + dakoda);
+    $('#profile-pic').attr('src', 'img/cats/' + dakoda);
+
+  // var intro = introImages[]
 
   var height = $(window).height();
-    $('#about').height(height);
+    $('#intro').height(height);
     $('#profile').height(height);
     $('#contact').height(height);
 
   $(window).resize(function() {
       var height = $(window).height();
-      $('#about').height(height);
+      $('#intro').height(height);
       $('#profile').height(height);
       $('#contact').height(height);
   });
+
+  // var introImages = ['co.jpg', 'door.jpg', 'kapula.jpg', 'kula.jpg', 'maui.jpg', 'patio.jpg', 'sidewalk.jpg'];
+
+  // function toggleStuff (){
+  //   // returns array of hidden images
+  //   var picArray = $('.li-imgs').find('img');
+  //   for (var i = 0; i > picArray.length; i++) {
+  //     // pluck off individual picture
+  //     var pic = picArray[i].pop();
+  //     // show that shiiiiit
+  //     pic.show();
+  //   }
+  // };
+
+  // setInterval(toggleStuff(), 3000)
+
+  var items = $('ul .li-imgs').toArray();
+  var totalIterations = (items.length - 1);
+  var i = 0;
+
+  setInterval(function() {
+    $(items[i]).fadeIn(2000).fadeOut(2000);
+
+    i = (i >= totalIterations) ? 0 : i + 1;
+  }, 4000)
+
 })
+
+
---
+++ about/server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Start a simple static server
+# To run, type `./server.sh` in
+# a terminal window or copy the
+# command below into a terminal
+# window while in the directory
+# you want to serve.
+#
+# To view site go to http://localhost:8000
+python -m SimpleHTTPServer 8000
