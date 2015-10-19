6aaf8c032118f8ff8e1bcf708f9aad61fcb5273e
Dakoda Wogan <d.dakoda@yahoo.com>
2015-10-19 19:17:07 -0400

    a circle became an oval and we've come full circle

--- about/javascripts/home.js
+++ about/javascripts/home.js
@@ -1,3 +1,4 @@
+// #### Hamburger Menu ####
 $(document).ready(function(){
   $('.hamburger').hide();
 
@@ -6,49 +7,35 @@ $(document).ready(function(){
     $('.hamburger').slideToggle("expand");
   });
 
+
+// #### Cat Change On Refresh ####
   var dakodaImages = ['milo.jpg', 'pi.jpg', 'ak.jpg', 'steven.jpg'];
   var dakoda = dakodaImages[Math.floor(Math.random() * dakodaImages.length)];
     $('#profile-pic').attr('src', 'img/cats/' + dakoda);
 
-  // var intro = introImages[]
-
+// #### Resize/Restore ####
   var height = $(window).height();
     $('#intro').height(height);
     $('#profile').height(height);
     $('#contact').height(height);
 
   $(window).resize(function() {
-      var height = $(window).height();
+    var height = $(window).height();
       $('#intro').height(height);
       $('#profile').height(height);
       $('#contact').height(height);
   });
 
-  // var introImages = ['co.jpg', 'door.jpg', 'kapula.jpg', 'kula.jpg', 'maui.jpg', 'patio.jpg', 'sidewalk.jpg'];
-
-  // function toggleStuff (){
-  //   // returns array of hidden images
-  //   var picArray = $('.li-imgs').find('img');
-  //   for (var i = 0; i > picArray.length; i++) {
-  //     // pluck off individual picture
-  //     var pic = picArray[i].pop();
-  //     // show that shiiiiit
-  //     pic.show();
-  //   }
-  // };
-
-  // setInterval(toggleStuff(), 3000)
-
+// #### JS To Toggle Intro Images ####
   var items = $('ul .li-imgs').toArray();
   var totalIterations = (items.length - 1);
   var i = 0;
 
   setInterval(function() {
-    $(items[i]).fadeIn(2000).fadeOut(2000);
+    $(items[i]).fadeIn(3000).fadeOut(3000);
 
     i = (i >= totalIterations) ? 0 : i + 1;
-  }, 4000)
+  }, 6000)
 
 })
-
 
